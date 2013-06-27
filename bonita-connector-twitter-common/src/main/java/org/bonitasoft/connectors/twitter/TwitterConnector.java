a0e77b9af7e0be39ac2a7d941cff705935dee863
Frederic Bouquet <frederic.bouquet@bonitasoft.com>
2013-06-27 13:02:11 +0000

    applied Maxence's patch

---
+++ bonita-connector-twitter-common/src/main/java/org/bonitasoft/connectors/twitter/TwitterConnector.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (C) 2009-2011 BonitaSoft S.A.
+ * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.connectors.twitter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.bonitasoft.engine.connector.Connector;
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * This connector provides a twitter sending service.
+ * 
+ * @author Matthieu Chaffotte
+ * @author Yanyan Liu
+ * @author Baptiste Mesta
+ * @author Haris Subašić
+ * @author Maxence Raoux
+ */
+
+public abstract class TwitterConnector implements Connector {
+
+	private String proxyHost;
+
+	private Integer proxyPort;
+
+	private String proxyUser;
+
+	private String proxyPass;
+
+	private String consumerKey;
+
+	private String consumerSecret;
+
+	private String accessToken;
+
+	private String accessTokenSecret;
+
+	@Override
+	public void setInputParameters(final Map<String, Object> parameters) {
+		final Object proxyHostObject = parameters.get("proxyHost");
+		proxyHost = proxyHostObject != null ? (String) proxyHostObject : "";
+		final Object proxyPortObject = parameters.get("proxyPort");
+		proxyPort = proxyPortObject != null ? (Integer) proxyPortObject : 0;
+		final Object proxyUserObject = parameters.get("proxyUser");
+		proxyUser = proxyUserObject != null ? (String) proxyUserObject : "";
+		final Object proxyPassObject = parameters.get("proxyPass");
+		proxyPass = proxyPassObject != null ? (String) proxyPassObject : "";
+		final Object consumerKeyObject = parameters.get("consumerKey");
+		consumerKey = consumerKeyObject != null ? (String) consumerKeyObject
+				: "";
+		final Object consumerSecretObject = parameters.get("consumerSecret");
+		consumerSecret = consumerSecretObject != null ? (String) consumerSecretObject
+				: "";
+		final Object accessTokenObject = parameters.get("accessToken");
+		accessToken = accessTokenObject != null ? (String) accessTokenObject
+				: "";
+		final Object accessTokenSecretObject = parameters
+				.get("accessTokenSecret");
+		accessTokenSecret = accessTokenSecretObject != null ? (String) accessTokenSecretObject
+				: "";
+
+	}
+
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+		final List<String> errors = new ArrayList<String>(1);
+
+		if (proxyPort < 0) {
+			errors.add("proxyPort cannot be less than 0!");
+		} else if (proxyPort > 65535) {
+			errors.add("proxyPort cannot be greater than 65535!");
+		}
+
+		if (!errors.isEmpty()) {
+			throw new ConnectorValidationException(this, errors);
+		}
+	}
+
+	@Override
+	public Map<String, Object> execute() throws ConnectorException {
+
+		final ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+		if (proxyHost != null && proxyPort != null) {
+			configurationBuilder.setHttpProxyHost(proxyHost);
+			configurationBuilder.setHttpProxyPort(proxyPort);
+			if (proxyUser != null && proxyPass != null) {
+				configurationBuilder.setHttpProxyUser(proxyUser);
+				configurationBuilder.setHttpProxyPassword(proxyPass);
+			}
+		}
+		configurationBuilder.setOAuthConsumerKey(consumerKey);
+		configurationBuilder.setOAuthConsumerSecret(consumerSecret);
+		configurationBuilder.setOAuthAccessToken(accessToken);
+		configurationBuilder.setOAuthAccessTokenSecret(accessTokenSecret);
+
+		try {
+			final TwitterFactory tf = new TwitterFactory(
+					configurationBuilder.build());
+			final Twitter twitter = tf.getInstance();
+			executeTask(twitter);
+		} catch (final Exception e) {
+			System.out.println(e.getMessage());
+			throw new ConnectorException(e.getMessage());
+		}
+		return null;
+	}
+
+	protected abstract void executeTask(Twitter twitter) throws Exception;
+
+	@Override
+	public void connect() throws ConnectorException {
+	}
+
+	@Override
+	public void disconnect() throws ConnectorException {
+	}
+}
---
+++ bonita-connector-twitter-direct-impl/src/main/java/org/bonitasoft/connectors/twitter/TwitterDirectMessageConnector.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 20012 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.connectors.twitter;
+
+import java.util.Map;
+
+import twitter4j.Twitter;
+
+/**
+ * @author Matthieu Chaffotte
+ * @author Haris Subašić
+ */
+public class TwitterDirectMessageConnector extends TwitterConnector {
+
+	private String message;
+
+	private String recipientId;
+
+	@Override
+	public void setInputParameters(final Map<String, Object> parameters) {
+		super.setInputParameters(parameters);
+		final Object messageObject = parameters.get("message");
+		message = messageObject != null ? (String) messageObject : "";
+		final Object recipientIdObject = parameters.get("recipientId");
+		recipientId = recipientIdObject != null ? (String) recipientIdObject
+				: "";
+	}
+
+	@Override
+	protected void executeTask(final Twitter twitter) throws Exception {
+		twitter.sendDirectMessage(recipientId, message);
+	}
+
+}
